f18f777c51bbe2887018684b6de7f6bbb23b950f
Ember Doyle <doyle.ember@gmail.com>
2024-11-04 10:39:24 -0700

    update port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
